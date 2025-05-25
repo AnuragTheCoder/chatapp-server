3d2fd23bb354e6e0ea032ee09f6748add65e2a5e
AnuragTheCoder <129210052+AnuragTheCoder@users.noreply.github.com>
2025-05-25 20:35:11 +0530

    Added complete backend

---
+++ constants/config.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+const CHATTU_TOKEN = "chattu-token";
+
+export { corsOptions, CHATTU_TOKEN };
